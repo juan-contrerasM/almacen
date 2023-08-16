b8294c1985d92c74db351187b444490470dd9a71
juan-contrerasM <115199779+juan-contrerasM@users.noreply.github.com>
2023-08-16 03:24:28 -0500

    Add files via upload

--- almacen/src/main/java/co/edu/uniquindio/almacen/Application.java
+++ almacen/src/main/java/co/edu/uniquindio/almacen/Application.java
@@ -1,12 +1,15 @@
 package co.edu.uniquindio.almacen;
 
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Application extends javafx.application.Application {
+    private Stage primaryStage;
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("menu.fxml"));
@@ -14,6 +17,23 @@ public class Application extends javafx.application.Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
+    }
+    public void loadingWindowProducts() {
+
+        try {
+            FXMLLoader load = new FXMLLoader(getClass().getResource("products.fxml"));
+            Parent root = load.load();
+            Scene escena = new Scene(root);
+            primaryStage = new Stage();
+            primaryStage.setResizable(false);
+            primaryStage.initModality(Modality.APPLICATION_MODAL);
+            primaryStage.setScene(escena);
+            primaryStage.setTitle("ventana de resgistro");
+            primaryStage.showAndWait();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
     }
 
--- almacen/src/main/java/co/edu/uniquindio/almacen/controller/ProductsController.java
+++ almacen/src/main/java/co/edu/uniquindio/almacen/controller/ProductsController.java
@@ -1,5 +1,66 @@
 package co.edu.uniquindio.almacen.controller;
+import co.edu.uniquindio.almacen.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
 
 public class ProductsController {
+        @FXML
+        private TextField textCodeApprobation;
+
+        @FXML
+        private TextField txtCant;
+
+        @FXML
+        private TextField txtCode;
+
+        @FXML
+        private TextField txtContainerWeight;
+
+        @FXML
+        private TextField txtExpirationDate;
+
+        @FXML
+        private TextField txtName;
+
+        @FXML
+        private TextField txtPackagingDate;
+
+        @FXML
+        private TextField txtTemperature;
+
+        @FXML
+        private TextField txtValue;
+
+        @FXML
+        void Update(ActionEvent event) {
+
+        }
+
+        @FXML
+        void consult(ActionEvent event) {
+
+        }
+
+        @FXML
+        void create(ActionEvent event) {
+
+        }
+
+        @FXML
+        void delete(ActionEvent event) {
+
+        }
+
+        @FXML
+        void openManu(ActionEvent event) {
+
+        }
 
 }
+
+
+
